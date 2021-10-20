66124b1192e51e661c313c7b2044ed99262b06c4
ainayeem <ain.nayeem1999@gmail.com>
2021-10-20 23:39:33 +0600

    first commit

---
+++ src/Context/FIrebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDHKsh7eKWKGuGXzC2e4_l13V5wIc9cLyM",
+  authDomain: "myhealth-21085.firebaseapp.com",
+  projectId: "myhealth-21085",
+  storageBucket: "myhealth-21085.appspot.com",
+  messagingSenderId: "627352179499",
+  appId: "1:627352179499:web:16feab2c6c6c89cbe5eb6e",
+};
+export default firebaseConfig;
